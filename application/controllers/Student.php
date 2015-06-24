5c757b9fe22be456ee9a1afdb33d5c70764338cf
akmnahid <mail@akmnahid.com>
2015-06-24 19:16:58 +0600

    #4 sections Fix Educator Label

--- application/controllers/Student.php
+++ application/controllers/Student.php
@@ -113,7 +113,7 @@ WHERE
                                         'type'  =>  'set',
                                         'set'   =>  [10,25,50,100,200,500]
                                     ],
-                                'default'   =>  50,
+                                'default'   =>   (!$this->input->get('filter[Result]')) ? 3 : $this->input->get('filter[Result]'),
                                 'label'     =>  'Results',
                                 'type'      =>  'dropdown',
                                 'bindDatabase'  => false,
--- application/views/Grades/index.php
+++ application/views/Grades/index.php
@@ -4,3 +4,26 @@
         <br/><br/>
     </div>
 </div>
+<div class="row">
+    <div class="col-md-12">
+        <?php Easol_Widget::show("DataTableWidget",
+            [
+                'query' => $query,
+                'pagination' => $pagination,
+                'filter'    =>  $filter,
+                'colOrderBy'    =>  $colOrderBy,
+                'colGroupBy'    =>  $colGroupBy,
+                'columns'   =>
+                    [
+                        ['name' => 'CourseTitle', 'title' => 'Course Name'],
+                        ['name' => 'LocalCourseCode', 'title' => 'Section Name'],
+                        ['name' => 'ClassPeriodName', 'title' => 'Period'],
+                        ['name' => 'Term', 'title' => 'Term'],
+                        ['name' => 'FirstName', 'title' => 'Educator','value' => function($model){ return $model->FirstName.' '.$model->LastSurname;  }],
+                        ['name' => 'StudentCount', 'title' => 'Students' ]
+                    ]
+            ]
+
+        ) ?>
+    </div>
+</div>
