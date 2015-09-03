33ece018f687cd60a1bb2b2c6b4a414c5b0493a6
Regis Camimura <regiscamimura@gmail.com>
2015-09-03 18:39:36 -0300

    57: Change EASOL Login from "Username" to "Email" - Modified the field's label and input name to "email" - Added a function to retrieve a record by email

--- application/views/Home/login.php
+++ application/views/Home/login.php
@@ -15,9 +15,9 @@
                 ?>
                 <form method="post" class="form-horizontal">
                     <div class="form-group">
-                        <label class="control-label col-md-1 col-sm-3">Username:</label>
+                        <label class="control-label col-md-1 col-sm-3">Email:</label>
                         <div class="col-md-4 col-sm-8">
-                            <input type="text" id="login" class="form-control" name="login[username]" value="<?= $this->input->post('login[username]') ?>"/>
+                            <input type="email" required id="login" class="form-control" name="login[email]" value="<?= $this->input->post('login[email]') ?>"/>
                         </div>
                     </div>
 
