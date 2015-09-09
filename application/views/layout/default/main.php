27e2c1152a8bc93e833f3ad28439af9801fbfee9
Edgar <edgar.f91@gmail.com>
2015-09-09 16:17:47 +0300

    debugging

--- application/views/layout/default/main.php
+++ application/views/layout/default/main.php
@@ -35,6 +35,10 @@
     <script src="https://apis.google.com/js/platform.js" async defer></script>
     <script>
   function signOut() {
+     gapi.load('auth2', function() {
+        gapi.auth2.init();
+      });
+    
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
       console.log('User signed out.');
