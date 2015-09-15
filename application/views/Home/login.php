d6faaeddecf0480949c6404280f08e87deb43d44
Anwar Baksh <pro@webdevstar.com>
2015-09-15 16:24:58 -0400

    login field label fix
    labels were set at 8% width by boostrap - tried a fix that may work

--- application/views/Home/login.php
+++ application/views/Home/login.php
@@ -16,21 +16,21 @@
                 <div class="alert alert-danger" id="google_ajax_error"></div>
                 <form method="post" class="form-horizontal">
                     <div class="form-group">
-                        <label class="control-label col-md-1 col-sm-3">Username:</label>
+                        <label class="control-label loginLabel">Username:</label>
                         <div class="col-md-4 col-sm-8">
                             <input type="text" id="login" class="form-control" name="login[username]" value="<?= $this->input->post('login[username]') ?>"/>
                         </div>
                     </div>
 
                     <div class="form-group">
-                        <label class="control-label col-md-1 col-sm-3">Password:</label>
+                        <label class="control-label loginLabel">Password:</label>
                         <div class="col-md-4 col-sm-8">
                             <input class="form-control" type="password" name="login[password]"/>
                         </div>
                     </div>
 
                     <div class="form-group">
-                        <div class="col-md-5 col-sm-11">
+                        <div class="col-md-5 col-sm-11 nudgright">
                             <button class="btn btn-primary pull-right" type="submit">Login</button><br /><br />
                             <button class="g-signin2 altSIbut pull-right" data-onsuccess="onSignIn" data-theme="dark"></button>
                         </div>
