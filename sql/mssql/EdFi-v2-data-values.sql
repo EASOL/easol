ac54214b58bba753cc28767d7f018c9397098edb
Jason Hoekstra <jasonhoekstra@gmail.com>
2015-10-03 12:30:56 -0400

    separate sql scripts by type

---
+++ sql/mssql/EdFi-v2-data-values.sql
@@ -0,0 +1,118 @@
+-- ----------------------------
+-- Records of AcademicHonorCategoryType
+-- ----------------------------
+SET IDENTITY_INSERT [edfi].[AcademicHonorCategoryType] ON
+GO
+INSERT INTO [edfi].[AcademicHonorCategoryType] ([AcademicHonorCategoryTypeId], [CodeValue], [Description], [ShortDescription], [Id], [LastModifiedDate], [CreateDate]) VALUES (N'1', N'Honor roll', N'Honor roll', N'Honor roll', N'E95D7A40-1341-4537-A47F-A5A648579EDC', N'2014-11-12 14:54:24.157', N'2014-11-12 14:54:25.570')
+GO
+GO
+INSERT INTO [edfi].[AcademicHonorCategoryType] ([AcademicHonorCategoryTypeId], [CodeValue], [Description], [ShortDescription], [Id], [LastModifiedDate], [CreateDate]) VALUES (N'2', N'Honor society', N'Honor society', N'Honor society', N'FD38F519-734B-4CD3-A764-EBC8DEC29580', N'2014-11-12 14:54:24.157', N'2014-11-12 14:54:25.570')
+GO
+GO
+INSERT INTO [edfi].[AcademicHonorCategoryType] ([AcademicHonorCategoryTypeId], [CodeValue], [Description], [ShortDescription], [Id], [LastModifiedDate], [CreateDate]) VALUES (N'3', N'Honorable mention', N'Honorable mention', N'Honorable mention', N'D6879260-5C2D-4274-A733-15AD5D252FC0', N'2014-11-12 14:54:24.157', N'2014-11-12 14:54:25.570')
+GO
+GO
+INSERT INTO [edfi].[AcademicHonorCategoryType] ([AcademicHonorCategoryTypeId], [CodeValue], [Description], [ShortDescription], [Id], [LastModifiedDate], [CreateDate]) VALUES (N'4', N'Honors program', N'Honors program', N'Honors program', N'41630AA6-79A4-469E-8445-4DE6757A13B0', N'2014-11-12 14:54:24.157', N'2014-11-12 14:54:25.570')
+GO
+GO
+INSERT INTO [edfi].[AcademicHonorCategoryType] ([AcademicHonorCategoryTypeId], [CodeValue], [Description], [ShortDescription], [Id], [LastModifiedDate], [CreateDate]) VALUES (N'5', N'Prize awards', N'Prize awards', N'Prize awards', N'58C42C6C-5DCC-4D8F-8A45-4D437273212B', N'2014-11-12 14:54:24.157', N'2014-11-12 14:54:25.570')
+GO
+GO
+INSERT INTO [edfi].[AcademicHonorCategoryType] ([AcademicHonorCategoryTypeId], [CodeValue], [Description], [ShortDescription], [Id], [LastModifiedDate], [CreateDate]) VALUES (N'6', N'Scholarships', N'Scholarships', N'Scholarships', N'E94BE722-3E77-4214-9916-4F9BB2DB2DB0', N'2014-11-12 14:54:24.157', N'2014-11-12 14:54:25.570')
+GO
+GO
+INSERT INTO [edfi].[AcademicHonorCategoryType] ([AcademicHonorCategoryTypeId], [CodeValue], [Description], [ShortDescription], [Id], [LastModifiedDate], [CreateDate]) VALUES (N'7', N'Awarding of units of value', N'Awarding of units of value', N'Awarding of units of value', N'4B0AE50C-EDDB-43F9-A95D-4A6991D12892', N'2014-11-12 14:54:24.157', N'2014-11-12 14:54:25.570')
+GO
+GO
+INSERT INTO [edfi].[AcademicHonorCategoryType] ([AcademicHonorCategoryTypeId], [CodeValue], [Description], [ShortDescription], [Id], [LastModifiedDate], [CreateDate]) VALUES (N'8', N'Citizenship award/recognition, but no units of val', N'Citizenship award/recognition, but no units of value awarded', N'Citizenship award/recognition, but no units of val', N'B439087F-F236-4FE9-9B95-1A9916E45D9D', N'2014-11-12 14:54:24.157', N'2014-11-12 14:54:25.570')
+GO
+GO
+INSERT INTO [edfi].[AcademicHonorCategoryType] ([AcademicHonorCategoryTypeId], [CodeValue], [Description], [ShortDescription], [Id], [LastModifiedDate], [CreateDate]) VALUES (N'9', N'Completion of requirement', N'Completion of requirement', N'Completion of requirement', N'7DF0276B-3C2F-472B-B97B-30DD40CC2715', N'2014-11-12 14:54:24.157', N'2014-11-12 14:54:25.570')
+GO
+GO
+INSERT INTO [edfi].[AcademicHonorCategoryType] ([AcademicHonorCategoryTypeId], [CodeValue], [Description], [ShortDescription], [Id], [LastModifiedDate], [CreateDate]) VALUES (N'10', N'Attendance award', N'Attendance award', N'Attendance award', N'D9149270-B5BE-49EE-9C7D-1988748ABA4B', N'2014-11-12 14:54:24.157', N'2014-11-12 14:54:25.570')
+GO
+GO
+INSERT INTO [edfi].[AcademicHonorCategoryType] ([AcademicHonorCategoryTypeId], [CodeValue], [Description], [ShortDescription], [Id], [LastModifiedDate], [CreateDate]) VALUES (N'11', N'Certificate', N'Certificate', N'Certificate', N'E7742389-4BC6-44AA-95B7-A24ABC47AA9E', N'2014-11-12 14:54:24.157', N'2014-11-12 14:54:25.570')
+GO
+GO
+INSERT INTO [edfi].[AcademicHonorCategoryType] ([AcademicHonorCategoryTypeId], [CodeValue], [Description], [ShortDescription], [Id], [LastModifiedDate], [CreateDate]) VALUES (N'12', N'Honor award', N'Honor award', N'Honor award', N'AFA4C565-1D5A-4939-A409-5612064B640D', N'2014-11-12 14:54:24.157', N'2014-11-12 14:54:25.570')
+GO
+GO
+INSERT INTO [edfi].[AcademicHonorCategoryType] ([AcademicHonorCategoryTypeId], [CodeValue], [Description], [ShortDescription], [Id], [LastModifiedDate], [CreateDate]) VALUES (N'13', N'Letter of student commendation', N'Letter of student commendation', N'Letter of student commendation', N'6912E90A-77D2-4F55-9A07-F7FD3C3F8035', N'2014-11-12 14:54:24.157', N'2014-11-12 14:54:25.570')
+GO
+GO
+INSERT INTO [edfi].[AcademicHonorCategoryType] ([AcademicHonorCategoryTypeId], [CodeValue], [Description], [ShortDescription], [Id], [LastModifiedDate], [CreateDate]) VALUES (N'14', N'Medals', N'Medals', N'Medals', N'128C0DCC-5E81-44F7-A24D-3B510F8038B0', N'2014-11-12 14:54:24.157', N'2014-11-12 14:54:25.570')
+GO
+GO
+INSERT INTO [edfi].[AcademicHonorCategoryType] ([AcademicHonorCategoryTypeId], [CodeValue], [Description], [ShortDescription], [Id], [LastModifiedDate], [CreateDate]) VALUES (N'15', N'National Merit scholar', N'National Merit scholar', N'National Merit scholar', N'12F03F6E-AE1B-414E-99E4-4BC994BB6581', N'2014-11-12 14:54:24.157', N'2014-11-12 14:54:25.570')
+GO
+GO
+INSERT INTO [edfi].[AcademicHonorCategoryType] ([AcademicHonorCategoryTypeId], [CodeValue], [Description], [ShortDescription], [Id], [LastModifiedDate], [CreateDate]) VALUES (N'16', N'Points', N'Points', N'Points', N'6F21C81D-2820-41B2-978F-1EF3451376CC', N'2014-11-12 14:54:24.157', N'2014-11-12 14:54:25.570')
+GO
+GO
+INSERT INTO [edfi].[AcademicHonorCategoryType] ([AcademicHonorCategoryTypeId], [CodeValue], [Description], [ShortDescription], [Id], [LastModifiedDate], [CreateDate]) VALUES (N'17', N'Promotion or advancement', N'Promotion or advancement', N'Promotion or advancement', N'B25AA1C8-A255-4306-97C5-A0675CE81B8F', N'2014-11-12 14:54:24.157', N'2014-11-12 14:54:25.570')
+GO
+GO
+INSERT INTO [edfi].[AcademicHonorCategoryType] ([AcademicHonorCategoryTypeId], [CodeValue], [Description], [ShortDescription], [Id], [LastModifiedDate], [CreateDate]) VALUES (N'18', N'Other', N'Other', N'Other', N'A6D4C3AD-1DD3-4965-92A8-6AC1EFF24631', N'2014-11-12 14:54:24.157', N'2014-11-12 14:54:25.570')
+GO
+GO
+SET IDENTITY_INSERT [edfi].[AcademicHonorCategoryType] OFF
+GO
+
+
+-- ----------------------------
+-- Records of AcademicSubjectDescriptor
+-- ----------------------------
+INSERT INTO [edfi].[AcademicSubjectDescriptor] ([AcademicSubjectDescriptorId], [AcademicSubjectTypeId]) VALUES (N'1', N'2')
+GO
+GO
+INSERT INTO [edfi].[AcademicSubjectDescriptor] ([AcademicSubjectDescriptorId], [AcademicSubjectTypeId]) VALUES (N'2', N'1')
+GO
+GO
+INSERT INTO [edfi].[AcademicSubjectDescriptor] ([AcademicSubjectDescriptorId], [AcademicSubjectTypeId]) VALUES (N'3', N'3')
+GO
+GO
+INSERT INTO [edfi].[AcademicSubjectDescriptor] ([AcademicSubjectDescriptorId], [AcademicSubjectTypeId]) VALUES (N'4', N'10')
+GO
+GO
+INSERT INTO [edfi].[AcademicSubjectDescriptor] ([AcademicSubjectDescriptorId], [AcademicSubjectTypeId]) VALUES (N'5', N'7')
+GO
+GO
+INSERT INTO [edfi].[AcademicSubjectDescriptor] ([AcademicSubjectDescriptorId], [AcademicSubjectTypeId]) VALUES (N'6', N'5')
+GO
+GO
+INSERT INTO [edfi].[AcademicSubjectDescriptor] ([AcademicSubjectDescriptorId], [AcademicSubjectTypeId]) VALUES (N'7', N'6')
+GO
+GO
+INSERT INTO [edfi].[AcademicSubjectDescriptor] ([AcademicSubjectDescriptorId], [AcademicSubjectTypeId]) VALUES (N'8', N'11')
+GO
+GO
+INSERT INTO [edfi].[AcademicSubjectDescriptor] ([AcademicSubjectDescriptorId], [AcademicSubjectTypeId]) VALUES (N'9', N'12')
+GO
+GO
+INSERT INTO [edfi].[AcademicSubjectDescriptor] ([AcademicSubjectDescriptorId], [AcademicSubjectTypeId]) VALUES (N'10', N'4')
+GO
+GO
+INSERT INTO [edfi].[AcademicSubjectDescriptor] ([AcademicSubjectDescriptorId], [AcademicSubjectTypeId]) VALUES (N'11', N'14')
+GO
+GO
+INSERT INTO [edfi].[AcademicSubjectDescriptor] ([AcademicSubjectDescriptorId], [AcademicSubjectTypeId]) VALUES (N'12', N'32')
+GO
+GO
+INSERT INTO [edfi].[AcademicSubjectDescriptor] ([AcademicSubjectDescriptorId], [AcademicSubjectTypeId]) VALUES (N'13', N'13')
+GO
+GO
+INSERT INTO [edfi].[AcademicSubjectDescriptor] ([AcademicSubjectDescriptorId], [AcademicSubjectTypeId]) VALUES (N'14', N'15')
+GO
+GO
+INSERT INTO [edfi].[AcademicSubjectDescriptor] ([AcademicSubjectDescriptorId], [AcademicSubjectTypeId]) VALUES (N'15', N'31')
+GO
+GO
+INSERT INTO [edfi].[AcademicSubjectDescriptor] ([AcademicSubjectDescriptorId], [AcademicSubjectTypeId]) VALUES (N'338', N'33')
+GO
+GO
+INSERT INTO [edfi].[AcademicSubjectDescriptor] ([AcademicSubjectDescriptorId], [AcademicSubjectTypeId]) VALUES (N'344', N'8')
+GO
+GO
+
