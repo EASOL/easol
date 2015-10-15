678e42e2044b4dbb88fc6c5e73e10e55838b8406
Regis Camimura <regiscamimura@gmail.com>
2015-10-15 09:42:27 -0300

    Fix on Google Sign in

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,208 +1,209 @@
 $(function() {
 
-    if ($('table#manageusers').length) {
-        $('#manageusers').DataTable();
-    }
+	if ($('table#manageusers').length) {
+		$('#manageusers').DataTable();
+	}
 
-   if ($('table#manageschools').length) {
-        $('#manageschools').DataTable();
-    }
+	if ($('table#manageschools').length) {
+		$('#manageschools').DataTable();
+	}
 
-   if ($('table#manageschooldetails').length) {
-        $('#manageschooldetails').DataTable();
-    }
+	if ($('table#manageschooldetails').length) {
+		$('#manageschooldetails').DataTable();
+	}
 
-    $( "#filter-result" ).change(function() {
-        $("#filter-form-result").val($( "#filter-result" ).val());
-        $( "#dataGridFormFilter").submit();
-    });
+	$( "#filter-result" ).change(function() {
+		$("#filter-form-result").val($( "#filter-result" ).val());
+		$( "#dataGridFormFilter").submit();
+	});
 
 
-    $("[data-toggle='modal']").on('click', function(e) {
-    	e.preventDefault();
-    	var $that = $(this);
+	$("[data-toggle='modal']").on('click', function(e) {
+		e.preventDefault();
+		var $that = $(this);
 
-        var $loading = $(".loading", $that.attr('data-target')).fadeIn();
+		var $loading = $(".loading", $that.attr('data-target')).fadeIn();
 
-    	if ($(this).attr('data-post-data')) {
-    		var $form = $($(this).attr('data-post-data'));
-    		$.ajax({
-    			url: $(this).attr('data-href'),
-    			type: 'post',
-    			data: $form.serialize(),
-    			dataType: 'json',
-    			success: function(response) {
-                    $loading.fadeOut();
-    				$(".modal-ajax-content", $that.attr('data-target')).html(response.html);
-    			}
-    		})
-    	}
-    })
+		if ($(this).attr('data-post-data')) {
+			var $form = $($(this).attr('data-post-data'));
+			$.ajax({
+				url: $(this).attr('data-href'),
+				type: 'post',
+				data: $form.serialize(),
+				dataType: 'json',
+				success: function(response) {
+					$loading.fadeOut();
+					$(".modal-ajax-content", $that.attr('data-target')).html(response.html);
+				}
+			})
+		}
+	})
 
-    $('#content-query').focus(function(){
-    	var query = $(this);
-    	if (query.val() == 'search text')
-    		query.val('');
-    });
+	$('#content-query').focus(function(){
+		var query = $(this);
+		if (query.val() == 'search text')
+			query.val('');
+	});
 
-    /* Get the query string into an object for use in the defilter list */
-    var urlParams;
-    (window.onpopstate = function () {
-        var match,
-            pl     = /\+/g,  // Regex for replacing addition symbol with a space
-            search = /([^&=]+)=?([^&]*)/g,
-            decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
-            query  = window.location.search.substring(1);
+	/* Get the query string into an object for use in the defilter list */
+	var urlParams;
+	(window.onpopstate = function () {
+		var match,
+			pl     = /\+/g,  // Regex for replacing addition symbol with a space
+			search = /([^&=]+)=?([^&]*)/g,
+			decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+			query  = window.location.search.substring(1);
 
-        urlParams = {};
-        while (match = search.exec(query))
-           urlParams[decode(match[1])] = decode(match[2]);
-    })();
+		urlParams = {};
+		while (match = search.exec(query))
+			urlParams[decode(match[1])] = decode(match[2]);
+	})();
 
-    $('.filter_active').click(function () {
-        delete urlParams[this.value];
-        var newQuery = $.param(urlParams);
-        var baseUrl = [location.protocol, '//', location.host, location.pathname].join(''); 
-        window.location = baseUrl+'?'+newQuery;       
-    })
+	$('.filter_active').click(function () {
+		delete urlParams[this.value];
+		var newQuery = $.param(urlParams);
+		var baseUrl = [location.protocol, '//', location.host, location.pathname].join('');
+		window.location = baseUrl+'?'+newQuery;
+	})
 
-    /* Sample code for browser extension dev when handling content links */
-   var assignmentLink = $('a.extension'), target = 'chrome-extension://maifknjmjnafdaiohogiffkdaebomimn';
-   
-   /* Tell the extension what was the last active URL  */
-   /*if(window.parent.location.origin === 'chrome-extension://maifknjmjnafdaiohogiffkdaebomimn'){*/
-   	window.parent.postMessage({operation: 'changeUrl', location: window.location.href}, target);
- /*  }*/
-   
-    if (assignmentLink.length) {
-      // Hide links if a Google Classroom page is not open
-      $(window).on('message', function (event) {
-        if (event.originalEvent.origin === target && !event.originalEvent.data.isClassroomOpen) {
-          assignmentLink.hide();
-        }
-      });
-      window.parent.postMessage({operation: 'isClassroomOpen'}, target);
+	/* Sample code for browser extension dev when handling content links */
+	var assignmentLink = $('a.extension'), target = 'chrome-extension://maifknjmjnafdaiohogiffkdaebomimn';
 
-      // Click handler for entering fields into the new assignment form
-      assignmentLink.click(function(event){
-        /*  event.preventDefault();
-         var link   = $(this),
-         title      = link.attr('title'),
-         desc       = link.attr('description'),
-         href       = link.attr('href');*/
+	/* Tell the extension what was the last active URL  */
+	/*if(window.parent.location.origin === 'chrome-extension://maifknjmjnafdaiohogiffkdaebomimn'){*/
+	window.parent.postMessage({operation: 'changeUrl', location: window.location.href}, target);
+	/*  }*/
 
-        event.preventDefault();
-        var link = $(this);
-        var message = {title: link.attr('title'), desc: link.attr('description'), href: link.attr('href')};
-        window.parent.postMessage(message, target);
-      });
-    }
+	if (assignmentLink.length) {
+		// Hide links if a Google Classroom page is not open
+		$(window).on('message', function (event) {
+			if (event.originalEvent.origin === target && !event.originalEvent.data.isClassroomOpen) {
+				assignmentLink.hide();
+			}
+		});
+		window.parent.postMessage({operation: 'isClassroomOpen'}, target);
 
-    $("#main-menu #management").click(function(event) {
-    	event.preventDefault();
-    	var submenu = $('ul', $(this).parent());
-    	if (submenu.length && submenu.is(':visible')) {
-    		submenu.slideUp( "slow", function() {
-    		// Animation complete.
-    		});
-    	} else {
-    		submenu.slideDown( "slow", function() {
-    		// Animation complete.
-    		});
-    	}
-    });
+		// Click handler for entering fields into the new assignment form
+		assignmentLink.click(function(event){
+			/*  event.preventDefault();
+			 var link   = $(this),
+			 title      = link.attr('title'),
+			 desc       = link.attr('description'),
+			 href       = link.attr('href');*/
 
-    $( "#schoolFilter" ).change(function() {
-        var schoolId = $(this).val();
+			event.preventDefault();
+			var link = $(this);
+			var message = {title: link.attr('title'), desc: link.attr('description'), href: link.attr('href')};
+			window.parent.postMessage(message, target);
+		});
+	}
 
-        if (schoolId == 'all') {
+	$("#main-menu #management").click(function(event) {
+		event.preventDefault();
+		var submenu = $('ul', $(this).parent());
+		if (submenu.length && submenu.is(':visible')) {
+			submenu.slideUp( "slow", function() {
+				// Animation complete.
+			});
+		} else {
+			submenu.slideDown( "slow", function() {
+				// Animation complete.
+			});
+		}
+	});
 
-            $('select#staffusi option').show();
-            $('select#staffusi').val('');
+	$( "#schoolFilter" ).change(function() {
+		var schoolId = $(this).val();
 
-        }else {
+		if (schoolId == 'all') {
 
-            $('select#staffusi option[school != "'+schoolId+'"]').hide();
-            $('select#staffusi option[school = "'+schoolId+'"]').show();
-            $('select#staffusi option[school = "reset"]').show();
-            $('select#staffusi').val('');
-        }
+			$('select#staffusi option').show();
+			$('select#staffusi').val('');
 
-    });
+		}else {
 
-    $('a.usermanagement-index-delete').click(function(event) {
-        event.preventDefault();
-        if (confirm('Are you sure you want to delete this user?')) {
-            window.location = this.href;
-        }
+			$('select#staffusi option[school != "'+schoolId+'"]').hide();
+			$('select#staffusi option[school = "'+schoolId+'"]').show();
+			$('select#staffusi option[school = "reset"]').show();
+			$('select#staffusi').val('');
+		}
 
-    })
+	});
 
-    if ($('#usermanagement-addedit-authtype input:checkbox').length && $('#usermanagement-addedit-authtype input:checkbox').prop('checked')) {
-        $('#usermanagement-addedit-password').hide();
-    }
+	$('a.usermanagement-index-delete').click(function(event) {
+		event.preventDefault();
+		if (confirm('Are you sure you want to delete this user?')) {
+			window.location = this.href;
+		}
 
-    $('#usermanagement-addedit-authtype input:checkbox').click(function(event) {
-            $('#usermanagement-addedit-password').toggle(this.checked == false)
-    });
-     $('[data-toggle="tooltip"]').tooltip();
+	})
+
+	if ($('#usermanagement-addedit-authtype input:checkbox').length && $('#usermanagement-addedit-authtype input:checkbox').prop('checked')) {
+		$('#usermanagement-addedit-password').hide();
+	}
+
+	$('#usermanagement-addedit-authtype input:checkbox').click(function(event) {
+		$('#usermanagement-addedit-password').toggle(this.checked == false)
+	});
+	$('[data-toggle="tooltip"]').tooltip();
 
 });
 
 
- /* ADDED BY ANWAR BAKSH ON 09/2015 */
- /* GOOGLE SIGN IN: https://developers.google.com/identity/sign-in/ */
- function onSignIn(googleUser) { 
-    console.log(googleUser);
-		// Useful data for your client-side scripts:
-		var profile = googleUser.getBasicProfile();
-		//console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-		//console.log("Name: " + profile.getName());
-		//console.log("Image URL: " + profile.getImageUrl());
-		//console.log("Email: " + profile.getEmail());
-		// The ID token you need to pass to your backend:
-		
-		var id_token = googleUser.getAuthResponse().id_token;
-		var uemail = profile.getEmail();
-		var name = profile.getName();
-		console.log('Attempting Google Sign In');
-		console.log("Google Token Recieved: " + id_token);
-	
-		var localbe = 'http://localhost/easol/';
-		var devbe = 'http://easol-dev.azurewebsites.net/';
-		var livebe = '';
-		var gloginPosturl = devbe;
-	      
-		var xhr = new XMLHttpRequest();
-		xhr.open('POST', gloginPosturl);
-		xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        xhr.onload = function() {
-           if(xhr.responseText=="gloginValid") {
-               /* GOOD SIGN IN */
-               console.log('Signed in as: ' + profile.getName() );
-               /* alert(profile.getName() + ' is now logged in!');  alert(xhr.responseText);*/ 
-               window.location='student'; 
-           } else {
-               /* FAILED SIGN IN */
-               console.log('Signed In Error: ' + xhr.responseText);
-               /* 
-               * we set the error message server side in the session for display on the redirect. 
-               * we redirect to the current url but without resending the post data like window.location.reload() would do.
-               */
-               window.location.href = window.location.href; 
-          }
-        };
-		xhr.send('idtoken=' + id_token + '&uemail=' + uemail);
-      }
- //  END SIGN IN
- /* GOOGLE LOGIN - WELL WE NEED TO LOGOUT */
- /* Login button must be on same page as logout button - but hidden  - for this to work */
-      function signOut() { console.log('Attempting Google Logout');
-	    var auth2 = gapi.auth2.getAuthInstance();
-	    auth2.signOut().then(function () {
-	      console.log('User signed out.');
-	    });         
-        console.log('End Google Logout');
-      }
+/* ADDED BY ANWAR BAKSH ON 09/2015 */
+/* GOOGLE SIGN IN: https://developers.google.com/identity/sign-in/ */
+function onSignIn(googleUser) {
+	console.log(googleUser);
+	// Useful data for your client-side scripts:
+	var profile = googleUser.getBasicProfile();
+	//console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+	//console.log("Name: " + profile.getName());
+	//console.log("Image URL: " + profile.getImageUrl());
+	//console.log("Email: " + profile.getEmail());
+	// The ID token you need to pass to your backend:
 
-       
+	var id_token = googleUser.getAuthResponse().id_token;
+	var uemail = profile.getEmail();
+	var name = profile.getName();
+	console.log('Attempting Google Sign In');
+	console.log("Google Token Recieved: " + id_token);
+
+	var localbe = 'http://easol/';
+	var devbe = 'http://easol-dev.azurewebsites.net/';
+	var livebe = '';
+	var gloginPosturl = localbe;
+
+	var xhr = new XMLHttpRequest();
+	xhr.open('POST', gloginPosturl);
+	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	xhr.onload = function() {
+		if(xhr.responseText=="gloginValid") {
+			/* GOOD SIGN IN */
+			console.log('Signed in as: ' + profile.getName() );
+			/* alert(profile.getName() + ' is now logged in!');  alert(xhr.responseText);*/
+			window.location='student';
+		} else {
+			/* FAILED SIGN IN */
+			console.log('Signed In Error: ' + xhr.responseText);
+			signOut();
+			/*
+			 * we set the error message server side in the session for display on the redirect.
+			 * we redirect to the current url but without resending the post data like window.location.reload() would do.
+			 */
+			window.location.href = window.location.href;
+		}
+	};
+	xhr.send('idtoken=' + id_token + '&uemail=' + uemail);
+}
+//  END SIGN IN
+/* GOOGLE LOGIN - WELL WE NEED TO LOGOUT */
+/* Login button must be on same page as logout button - but hidden  - for this to work */
+function signOut() { console.log('Attempting Google Logout');
+	var auth2 = gapi.auth2.getAuthInstance();
+	auth2.signOut().then(function () {
+		console.log('User signed out.');
+	});
+	console.log('End Google Logout');
+}
+
+
