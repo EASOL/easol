947c760b314467e47b5a646d77a3aa7e62aaaa3e
Edgar <edgar.f91@gmail.com>
2015-11-12 13:14:41 +0200

    one label change

--- application/views/Analytics/index.php
+++ application/views/Analytics/index.php
@@ -1,7 +1,7 @@
 <?php extract($data); ?>
 <div class="row">
     <div class="col-md-12 col-sm-12">
-        <h1 class="page-header">Sections</h1>
+        <h1 class="page-header">Analytics</h1>
     </div>
 </div>
 <div class="row">
