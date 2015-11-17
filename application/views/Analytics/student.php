9723682b2500cbd7c5edc1ec65c519ec6f8ce0b7
x <vbsaltydog@gmail.com>
2015-11-16 23:20:54 -0500

    #152 Made requested changes to analytics::student() view.

--- application/views/Analytics/student.php
+++ application/views/Analytics/student.php
@@ -1,14 +1,14 @@
 <?php extract($data); ?>
 <div class="row">
     <div class="col-md-12 col-sm-12">
-        <h1 class="page-header">Analytics. Section:<?=$section->UniqueSectionCode?></h1>
+        <h1 class="page-header">Analytics</h1>
     </div>
 </div>
 <div class="row">
     <div class="col-md-12 col-sm-12">
         <div class="panel panel-default">
             <div class="panel-body">
-               <h2 class="backToH2">Details</h2>
+               <h2 class="backToH2"><?php echo $student->FirstName . ' ' . $student->MiddleName . ' ' . $student->LastSurname; ?></h2>
               <table class="table table-striped table-bordered" cellspacing="0" width="100%">
                 <thead>
                   <tr>
@@ -16,7 +16,6 @@
                     <th>Course</th>
                     <th>Term</th>
                     <th>Period</th>
-                    <th>Student</th>
                   </tr>
                 </thead>
                 <tbody>
@@ -25,7 +24,6 @@
                     <td><?php echo $section->LocalCourseCode; ?></td>                             
                     <td><?php echo $section->CodeValue; ?></td>                             
                     <td><?php echo $section->Period; ?></td>
-                    <td><?php echo $student->FirstName . ' ' . $student->MiddleName . ' ' . $student->LastSurname; ?></td>
                   </tr>
                 </tbody>
               </table>
