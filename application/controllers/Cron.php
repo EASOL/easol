4f76c7f440efd608b59bbb872839509615eaa548
x <vbsaltydog@gmail.com>
2015-11-18 00:55:48 -0500

    #170 Added cli interface for cron tasks and moved email encryption to a more appropriate codebase location.

---
+++ application/controllers/Cron.php
@@ -0,0 +1,37 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Cron extends CI_Controller {
+
+    public function __construct () 
+    {
+        parent::__construct(); 
+
+        if (!$this->input->is_cli_request()) {
+            exit('Unathorized Request.');
+        }
+    }
+
+    public function create_hashes () 
+    {            
+        set_time_limit(300); // kill after 5 minutes 
+        $this->load->library('Easol_Generic_L');
+
+        $students = $this->db->query("SELECT ElectronicMailAddress FROM edfi.StudentElectronicMail")->result();
+        foreach ($students as $student)
+        {
+            $email  = $student->ElectronicMailAddress;
+            $hash   = $this->easol_generic_l->encrypt_email($email);
+
+            $exists = $this->db->select("COUNT(*)")
+            ->from('easol.EmailLookup')
+            ->where('email', $email)
+            ->where('HashedEmail', $hash) 
+            ->count_all_results();
+
+            if ($exists === 0)
+                $this->db->insert('easol.EmailLookup', array('email' => $email, 'HashedEmail' => $hash));
+        }
+    }        
+
+}
