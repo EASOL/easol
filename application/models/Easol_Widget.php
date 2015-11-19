8f3afeb13521320498761e44a88ec1492bfa6e6a
shlag3n <oksana.melnikova@gmail.com>
2015-11-19 15:49:51 +0300

    try to fix Easol_Widget double file issue

--- application/models/Easol_Widget.php
+++ application/models/Easol_Widget.php
@@ -1,22 +1,11 @@
 <?php
-/**
- * User: Nahid Hossain
- * Email: mail@akmnahid.com
- * Phone: +880 172 7456 280
- * Date: 6/3/2015
- * Time: 2:43 PM
- */
-
 class Easol_Widget extends CI_Model {
-
     /**
      * constructor method
      */
     public function __construct(){
-
         parent::__construct();
     }
-
     /**
      * display the widget
      * @param $widgetClass
@@ -24,7 +13,6 @@ class Easol_Widget extends CI_Model {
      */
     public static function show($widgetClass,$param=[]){
             /* @var $obj Easol_BaseWidget */
-
             require_once APPPATH.'/widgets/'.$widgetClass.'/'.$widgetClass.'.php';
             $obj = new $widgetClass();
             foreach($param as $key => $value){
@@ -32,6 +20,4 @@ class Easol_Widget extends CI_Model {
             }
             $obj->run();
     }
-
-
 }
