f977f5ba5dc4a31d50d0891719a96857ef4ccad6
shlag3n <oksana.melnikova@gmail.com>
2015-11-19 13:16:08 +0300

    added changes from leaning lab to content extension

--- application/views/Content/index.php
+++ application/views/Content/index.php
@@ -117,7 +117,6 @@
                                   </span>
                                 <?php endif; ?>
                               <?php endforeach; ?>
-
                           <?php endforeach; ?>
                            </div>
                         </div>
