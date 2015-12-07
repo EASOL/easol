052157c5c571d33340381b38221ccf4e919b4756
tarlles10 <tarlles10@gmail.com>
2015-12-07 00:15:40 -0600

    Worked to set up PHPUnit - Reviewed the codes on Welcome_test in controllers for understand how the PHPUnit test works. - Worked on code in Reports_test in controllers to make the test pass until complete all routine without return any failures.

--- application/tests/controllers/Welcome_test.php
+++ application/tests/controllers/Welcome_test.php
@@ -13,7 +13,7 @@ class Welcome_test extends TestCase
 	public function test_index()
 	{
 		$output = $this->request('GET', ['Welcome', 'index']);
-		$this->assertContains('<title>Welcome to CodeIgniter</title>', $output);
+		$this->assertContains('Welcome', $output);
 	}
 
 	public function test_method_404()
