03bad6dca5815dc7367fe1173ca1bc0e2bddc803
Abraham Sánchez <abraham@twintags.com>
2015-12-07 17:28:39 +0100

    Add customized database.php suitable for the CI environment

---
+++ application/config/database.ci.php
@@ -0,0 +1,28 @@
+/* This file provides a default database setup for CI purposes */
+
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$active_group = 'mssql';
+$query_builder = TRUE;
+
+/* Connect MSSQL Server*/
+$db['mssql']['dsn'] = '';
+$db['mssql']['hostname'] = $_ENV['CI_DATABASE_HOSTNAME'];
+$db['mssql']['username'] = $_ENV['CI_DATABASE_USERNAME'];
+$db['mssql']['password'] = $_ENV['CI_DATABASE_PASSWORD'];
+$db['mssql']['database'] = $_ENV['CI_DATABASE_NAME'];
+$db['mssql']['dbdriver'] = 'odbc';
+$db['mssql']['dbprefix'] = '';
+$db['mssql']['pconnect'] = FALSE;
+$db['mssql']['db_debug'] = TRUE;
+$db['mssql']['cache_on'] = FALSE;
+$db['mssql']['cachedir'] = '';
+$db['mssql']['char_set'] = 'utf8';
+$db['mssql']['dbcollat'] = 'utf8_general_ci';
+$db['mssql']['swap_pre'] = '';
+$db['mssql']['autoinit'] = TRUE;
+$db['mssql']['stricton'] = FALSE;
+$db['mssql']['port'] = 1433;
+
+?>
