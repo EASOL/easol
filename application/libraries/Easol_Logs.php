4e62ea958b319529c8339fa55c9ba2840c52f0af
tarlles10 <tarlles10@gmail.com>
2015-12-08 18:25:02 -0600

    1-Worked on code about #159-feature-logging from Development branch - Implemented the codes at the method writeLog in Home controllers for keep the logout logs - Modified the codes at the method Log in Easol_Logs to check all IP Address of the machine

--- application/libraries/Easol_Logs.php
+++ application/libraries/Easol_Logs.php
@@ -9,7 +9,16 @@ Class Easol_Logs {
 		$data['StaffUSI'] = $_SESSION['StaffUSI'];
 		$data['Controller'] = $ci->router->fetch_class();
 		$data['Method'] = $ci->router->fetch_method();
-		$data['IpAddress'] = $_SERVER['REMOTE_ADDR'];//$ci->input->ip_address();
+                $http_client_ip = $_SERVER['HTTP_CLIENT_IP']; 
+                $http_x_forwarded_for = $_SERVER['HTTP_X_FORWARDED_FOR']; 
+                $remote_addr = $ci->input->ip_address(); 
+                if(!empty($http_client_ip)){
+                    $data['IpAddress']=$http_client_ip;
+                }else if(!empty($http_x_forwarded_for)){
+                    $data['IpAddress']=$http_x_forwarded_for;
+                } else {
+                    $data['IpAddress']=$remote_addr;
+                } 
                 $data['Data'] = json_encode($data['Data']);
 		$ci->db->insert('EASOL.Logs', $data);
 	}
