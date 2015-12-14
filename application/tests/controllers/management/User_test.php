6ab0e2cfaf4c51a74ed0ae371f80db5f01dadee3
tarlles10 <tarlles10@gmail.com>
2015-12-13 22:00:05 -0600

    #phpunit development of the tests with phpUnit over User/School Management. - Implemented the use case test in User_test.php method index, save and Edit for check values over the output in User/School Management.

--- application/tests/controllers/management/User_test.php
+++ application/tests/controllers/management/User_test.php
@@ -10,10 +10,83 @@ class User_test extends TestCase
 		$crawler = new Crawler($output);
 
 		$this->assertContains('<h1 class="page-header">User Management</h1>', $output);
-		$this->assertContains("207219", $output);
+                $this->assertContains('<button class="btn btn-primary pull-right pre-data-table">Add User</button>', $output);
+                $this->assertContains('CSV', $output);
+                
+                $this->assertContains('StaffUSI', $output);
+                $this->assertContains('Full Name', $output);
+                $this->assertContains('Role', $output);
+                $this->assertContains('Authorization Type', $output);
+                $this->assertContains('School', $output);
+                $this->assertContains('Actions', $output);
+                $this->assertContains('<div class="col-md-8 col-sm-8 txt-annotation">
+                    This computer system is the property of the the Center of Education Innovation. It is for authorized
+                    use only.
+                    Unauthorized or improper use of this system may result in civil charges and/or criminal penalties.
+                </div>', $output);
+	}
+        
+        public function test_save()
+	{
+		$output = $this->request('GET', 'management/user/save');
 
+                $crawler = new Crawler($output);
+
+		$this->assertContains('<h1 class="page-header">User Management</h1>', $output);
+                $this->assertContains('<label for="school">Filter by School</label>', $output);
+                $this->assertContains('<label for="staffusi">
+														StaffUSI Full Name
+						</label>', $output);
+                $this->assertContains('<label for="role">Role</label>', $output);
+                $this->assertContains('<label for="Password">
+														Password </label>', $output);
+                $this->assertContains('<div class="col-md-8 col-sm-8 txt-annotation">
+                    This computer system is the property of the the Center of Education Innovation. It is for authorized
+                    use only.
+                    Unauthorized or improper use of this system may result in civil charges and/or criminal penalties.
+                </div>', $output);
+                
+                $post = ['user' => 
+                    ['schoolFilter' => '255901', 
+                     'staffusi' => '207285', 
+                     'user[RoleId]' => '1', 
+                     'user[Password]' => '123456', 
+                     'user[GoogleAuth]' => '']
+                    ];
+                
+                $output = $this->request(
+                    'PUT', 'management/user/save', $post
+                );
+                
 	}
 
+        public function test_edit()
+	{
+		$output = $this->request('GET', 'management/user/save/207285');
+
+                $crawler = new Crawler($output);
+
+		$this->assertContains('<h1 class="page-header">User Management</h1>', $output);
+                $this->assertContains('<label for="school">Filter by School</label>', $output);
+                $this->assertContains('<label for="staffusi">
+														StaffUSI Full Name
+						</label>', $output);
+                $this->assertContains('<label for="staffEmail">StaffUSI Email</label>', $output);
+                $this->assertContains('<label for="role">Role</label>', $output);
+                $this->assertContains('<label for="Password">
+														Password </label>', $output);
+                $this->assertContains('<div class="col-md-8 col-sm-8 txt-annotation">
+                    This computer system is the property of the the Center of Education Innovation. It is for authorized
+                    use only.
+                    Unauthorized or improper use of this system may result in civil charges and/or criminal penalties.
+                </div>', $output);
+                
+                $this->assertContains('Grand Bend ISD', $output);                
+                $this->assertContains('David Wilson 207285', $output);
+                $this->assertContains('tarlles10@gmail.com', $output);
+                $this->assertContains('System Administrator', $output);
+	}
+        
 	public function setUp() {
 		//system("sqlcmd -S REGIS-PC\SQLEXPRESS -i ".APPPATH."tests/database.sql");
 	}
