dd1789976260582add9eb54dc1216bd86adf6fe5
tarlles10 <tarlles10@gmail.com>
2015-12-23 17:37:44 -0600

    Worked on issues PHPUnit Test Reports - Added Crawler library on test case Reports_test - Modified the "test_index" function on the Reports_test test case using Crawler Library for interface tests. - Added the elements of the "Dashboard Configuration" data in the interface test routine in "test_index" test of the case function "Reports_test". - Implemented the "test_add_post" routine in the test case to add new data of reports and then to redirect reports/index. - Adjusted a routine to force the inclusion of sample data in the create method of new reports.

--- application/tests/controllers/Reports_test.php
+++ application/tests/controllers/Reports_test.php
@@ -7,16 +7,70 @@
  * @copyright  2015 Kenji Suzuki
  * @link       https://github.com/kenjis/ci-phpunit-test
  */
-
+use Symfony\Component\DomCrawler\Crawler;
 class Reports_test extends TestCase
 {
+        /**
+         * @group report
+         * Tests the index page
+         */
 	public function test_index()
 	{
-		$output = $this->request('GET', 'reports/index');
-		$this->assertContains('<h1 class="page-header">Flex Reports</h1>', $output);
-		$this->assertContains('F&P ELA Mastery for 2nd Grade Students', $output);
+            $output = $this->request('GET', 'reports/index');
 
-	}
+            $crawler = new Crawler($output);
+            
+            $title = $crawler->filter('h1.page-header')->eq(0)->text();
+            $this->assertContains("Flex Reports", $title);
+            
+            $reportName = $crawler->filter('th:contains("Report Name")')->eq(0)->text();
+            $this->assertContains("Report Name", $reportName);
+            
+            $displayType = $crawler->filter('th:contains("Display Type")')->eq(0)->text();
+            $this->assertContains("Display Type", $displayType);
+            
+            $category = $crawler->filter('th:contains("Category")')->eq(0)->text();
+            $this->assertContains("Category", $category);
+
+            $school = $crawler->filter('th:contains("School")')->eq(0)->text();
+            $this->assertContains("School", $school);            
+            
+            $access = $crawler->filter('th:contains("Access")')->eq(0)->text();
+            $this->assertContains("Access", $access);
+            
+            $edit = $crawler->filter('th:contains("Edit")')->eq(0)->text();
+            $this->assertContains("Edit", $edit);
+            
+            $delete = $crawler->filter('th:contains("Delete")')->eq(0)->text();
+            $this->assertContains("Delete", $delete);
+            
+            $newFlexReport = $crawler->filter('a[class="btn btn-primary"]')->eq(0)->text();
+            $this->assertContains("New Flex Report", $newFlexReport);
+            
+            $title2 = $crawler->filter('h3.panel-title')->eq(0)->text();
+            $this->assertContains("Dashboard Configuration", $title2);
+            
+            $role = $crawler->filter('th:contains("Role")')->eq(0)->text();
+            $this->assertContains("Role", $role); 
+            
+            $leftChart = $crawler->filter('th:contains("Left Chart")')->eq(0)->text();
+            $this->assertContains("Left Chart", $leftChart); 
+            
+            $rightChart = $crawler->filter('th:contains("Right Chart")')->eq(0)->text();
+            $this->assertContains("Right Chart", $rightChart); 
+            
+            $bottomTable = $crawler->filter('th:contains("Bottom Table")')->eq(0)->text();
+            $this->assertContains("Bottom Table", $bottomTable); 
+            
+            $annotation = $crawler->filter('div.txt-annotation')->eq(0)->text();
+            $this->assertContains("
+                    This computer system is the property of the the Center of Education Innovation. It is for authorized
+                    use only.
+                    Unauthorized or improper use of this system may result in civil charges and/or criminal penalties.
+                ", $annotation);
+            
+            //var_dump($reportName);die();
+        }
 
 	public function test_view_bar_chart() {
 		$output = $this->request('GET', ['Reports', 'view', 13]);
@@ -41,5 +95,37 @@ class Reports_test extends TestCase
 		//system("sqlcmd -S REGIS-PC\SQLEXPRESS -i ".APPPATH."tests/database.sql");
 	}
 
+        /**
+         * @group report
+         * @group report-crud
+         * Tests the actual add report process
+         */
+        public function test_add_post() {
+
+              $post = [
+                  'report'=>[
+                       'ReportName' => 'F&P ELA Mastery',
+                       'ReportCategoryId' => '1',
+                       'CommandText' => 'SELECT  Version, COUNT(*) as StudentAssessments FROM edfi.Student '
+                                       . 'INNER JOIN edfi.StudentAssessment ON Student.StudentUSI = StudentAssessment.StudentUSI '
+                                       . 'INNER JOIN edfi.AcademicSubjectDescriptor ON StudentAssessment.AcademicSubjectDescriptorId = AcademicSubjectDescriptor.AcademicSubjectDescriptorId '
+                                       . 'INNER JOIN edfi.AcademicSubjectType ON AcademicSubjectDescriptor.AcademicSubjectTypeId = AcademicSubjectType.AcademicSubjectTypeId '
+                                       . 'WHERE CodeValue = \'Mathematics\' '
+                                       . 'GROUP BY CodeValue, Version '
+                                       . 'ORDER BY Version, CodeValue ',
+                       'LabelX' => 'Year',
+                       'LabelY' => 'Student Assesments',
+                       'ReportDisplayId' => '2',
+                       'CreatedBy' => '207219',
+                       'CreatedOn' => '2015-07-01 03:30:29.080',
+                       'UpdatedBy' => '207219',
+                       'UpdatedOn' => '2015-12-05 05:44:40.520',
+                       'SchoolId' => '255901001'
+                   ]
+              ];
+
+              $this->request('POST', 'reports/create', $post);
+              $this->assertRedirect('reports/index', 302);
+        }
 
 }
