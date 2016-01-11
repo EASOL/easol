6df3c09b6dbd9a0873cf6e7d67745d70d37f1c1b
Regis Camimura <regiscamimura@gmail.com>
2016-01-11 12:36:32 -0200

    Setup database connection for travis CI

--- application/config/database.ci.php
+++ application/config/database.ci.php
@@ -12,12 +12,12 @@ $active_group = 'testing';
 $query_builder = TRUE;
 
 $db['testing'] = array(
-    'dsn'   => '',
-    'hostname' => $_ENV['CI_DATABASE_HOSTNAME'],
-    'username' => $_ENV['CI_DATABASE_USERNAME'],
-    'password' => $_ENV['CI_DATABASE_PASSWORD'],
-    'database' => $_ENV['CI_DATABASE_NAME'],
-    'dbdriver' => 'odbc',
+    'dsn'   => "dblib:host=oqc2uoyejf.database.windows.net;dbname=easol_dev",
+    'hostname' => '', //$_ENV['CI_DATABASE_HOSTNAME'],
+    'username' => 'easol_dev_dba', //$_ENV['CI_DATABASE_USERNAME'],
+    'password' => '%Z!A8iVnH6e$OKMk', //$_ENV['CI_DATABASE_PASSWORD'],
+    'database' => '', //$_ENV['CI_DATABASE_NAME'],
+    'dbdriver' => 'pdo',
     'dbprefix' => '',
     'pconnect' => TRUE,
     'db_debug' => TRUE,
