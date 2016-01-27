4dacd707292fd2ccfac6b4ab0731705538669fea
Edgar F <edgar.f91@gmail.com>
2016-01-27 23:08:11 +0200

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -19,6 +19,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $config['base_url'] = '';
 
+
 /*
 |--------------------------------------------------------------------------
 | Index File
