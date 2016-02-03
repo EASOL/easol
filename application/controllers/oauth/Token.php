9f7e05ec8ae4c8a2e737ad20a5996ebaec81372c
Edgar <edgar.f91@gmail.com>
2016-02-04 00:02:29 +0200

    Small testing

--- application/controllers/oauth/Token.php
+++ application/controllers/oauth/Token.php
@@ -41,7 +41,7 @@ class Token extends CI_Controller
 		// client collects login credentials and posts them here along with the client details.
 
 		if (!empty($email) and !empty($password)) {
-			echo $email."|".$password."|";
+			
 			// Verify the user's username and password
 	        if($this->_login(array('email' => $email, 'password' => $password))) {
 				// Handle a request for an OAuth2.0 Access Token and send the response to the client
@@ -49,10 +49,10 @@ class Token extends CI_Controller
 	        }
 	    }
 
-	 /*exit(json_encode(array( 'error' 				=> 'login invalid',
-							'error_description'		=> 'The email and/or password is invalid.'
+		exit(json_encode(array( 'error' 				=> 'login invalid',
+								'error_description'		=> 'The email and/or password is invalid.|'.$email."|".$password."|";
 						))
-		);*/
+		);
 
 		// verification: if the client wants to verify that the code is not an arbitrary code as a forgery then they can simply
 		// test the code for verification at the resource server Resource.php
@@ -63,7 +63,6 @@ class Token extends CI_Controller
     {
         $this->load->model('Usermanagement_M');
         $user = $this->Usermanagement_M->getEasolUsers($data['email'], "SEM.ElectronicMailAddress");
-        var_dump($user);
         if(is_array($user) and !empty($user)) {
             $this->load->model('entities/easol/Easol_StaffAuthentication');
             $authentication = $this->Easol_StaffAuthentication->findOne(['StaffUSI' => $user[0]->StaffUSI]);
