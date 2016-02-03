56f412ad28a08a6526eb6b3ae970fa7b09250610
Steve Madison <vbsaltydog@gmail.com>
2016-02-02 22:25:34 -0500

    Added a blueprint file for the .net dev as client.html and set the oauth token controller to use the prefixed db tables, among other necessary changes.

--- application/controllers/oauth/Token.php
+++ application/controllers/oauth/Token.php
@@ -11,44 +11,48 @@ class Token extends CI_Controller
 		require_once(APPPATH.'third_party/oauth2-server-php/src/OAuth2/Autoloader.php');
 		OAuth2\Autoloader::register();
 
+		$config = array(
+            'client_table' 			=> 'EASOL.oauth_clients',
+            'access_token_table' 	=> 'EASOL.oauth_access_tokens',
+            'refresh_token_table' 	=> 'EASOL.oauth_refresh_tokens',
+            'code_table' 			=> 'EASOL.oauth_authorization_codes',
+            'user_table' 			=> 'EASOL.oauth_users',
+            'jwt_table'  			=> 'EASOL.oauth_jwt',
+            'jti_table'  			=> 'EASOL.oauth_jti',
+            'scope_table'  			=> 'EASOL.oauth_scopes',
+            'public_key_table'  	=> 'EASOL.oauth_public_keys',
+        );
+
 		// $dsn is the Data Source Name for your database, for exmaple "mysql:dbname=my_oauth2_db;host=localhost"
-		$storage = new OAuth2\Storage\Pdo(array('dsn' => 'sqlsrv:Server=oqc2uoyejf.database.windows.net,1433;Database=easol_dev', 'username' => 'easol_dev_dba@oqc2uoyejf', 'password' => '%Z!A8iVnH6e$OKMk'));
+		$storage = new OAuth2\Storage\Pdo(array('dsn' => 'sqlsrv:Server=oqc2uoyejf.database.windows.net,1433;Database=easol_dev', 'username' => 'easol_dev_dba@oqc2uoyejf', 'password' => '%Z!A8iVnH6e$OKMk'), $config);
 
 		// Pass a storage object or array of storage objects to the OAuth2 server class
 		$this->server = new OAuth2\Server($storage);
 
 		// Add the "Client Credentials" grant type (it is the simplest of the grant types)
-		$this->server->addGrantType(new OAuth2\GrantType\ClientCredentials($storage));		
-
-		// Add the "Authorization Code" grant type (this is where the oauth magic happens)
-		// $this->server->addGrantType(new OAuth2\GrantType\AuthorizationCode($storage));		
-
+		$this->server->addGrantType(new OAuth2\GrantType\ClientCredentials($storage));	
 	}
 
 	function index ()
 	{
-		$email 		= $this->input->post('email', NULL);
-		$password 	= $this->input->post('password', NULL);
+		$email 		= $this->input->post('email');
+		$password 	= $this->input->post('password');
 
 		// client collects login credentials and posts them here along with the client details.
-		// we attempt to validate the user and:
-		// error   : simply redirect the browser back to the sender url with an error message in the post for display.
-		// success : send the access code to a designated url at the client where it is processed to create the session on the client.  
 
-		if ($email and $password) {
-
+		if (!empty($email) and !empty($password)) {
+			
 			// Verify the user's username and password
-	        //if($this->_login(array('email' => $email, 'password' => $password))) {
+	        if($this->_login(array('email' => $email, 'password' => $password))) {
 				// Handle a request for an OAuth2.0 Access Token and send the response to the client
-				$this->server->handleTokenRequest(OAuth2\Request::createFromGlobals())->send();
-	        //}
-	        //else {
-				// redirect();
-	        //}
+				return $this->server->handleTokenRequest(OAuth2\Request::createFromGlobals())->send();
+	        }
+	    }
 
-		} else {
-			// redirect();
-		}
+		exit(json_encode(array( 'error' 				=> 'login invalid',
+								'error_description'		=> 'The email or password were not valid.'
+						))
+		);
 
 		// verification: if the client wants to verify that the code is not an arbitrary code as a forgery then they can simply
 		// test the code for verification at the resource server Resource.php
