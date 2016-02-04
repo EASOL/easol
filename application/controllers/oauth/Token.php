375aa076d2b9698306e45cfdaa91a645d76e655f
Steve Madison <vbsaltydog@gmail.com>
2016-02-03 22:56:14 -0500

    Added some more CORS supporting code.

--- application/controllers/oauth/Token.php
+++ application/controllers/oauth/Token.php
@@ -1,6 +1,13 @@
 <?php
 
-header('Access-Control-Allow-Origin: *'); // Allow cross domain and file:// request sources.
+// * might need to be set to the source domain since http auth is being used.
+header('Access-Control-Allow-Origin: *'); // Allow cross domain and file:// request sources. 
+header("Access-Control-Allow-Credentials: true"); // Allow cookies, authentication, ssl.
+
+// Handling the Preflight
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') { 
+    exit;
+}
 
 class Token extends CI_Controller 
 {
