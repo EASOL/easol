3f6eee55dd0d5c4787c841b07d99d664fce652d8
Regis Camimura <regiscamimura@gmail.com>
2016-03-08 12:07:47 -0300

    #273 - Modified how the parameter filters are used

--- application/helpers/report_helper.php
+++ application/helpers/report_helper.php
@@ -1,7 +1,7 @@
 <?php
 
 function report_filter_types() {
-	return ['Free Text'=>'Free Text', 'Static List'=>'Static List', 'Dynamic List'=>'Dynamic List', 'System Variable'=>'System Variable', 'Parameter'=>'Parameter'];
+	return ['Free Text'=>'Free Text', 'Static List'=>'Static List', 'Dynamic List'=>'Dynamic List', 'System Variable'=>'System Variable'];
 }
 
 function report_display_types() {
