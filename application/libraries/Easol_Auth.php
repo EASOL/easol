6c48289b807456424958f15bce37121a4b95bfa5
Regis Camimura <regiscamimura@gmail.com>
2016-04-20 09:44:09 -0300

    #315 - Added the new permission layer

--- application/libraries/Easol_Auth.php
+++ application/libraries/Easol_Auth.php
@@ -14,9 +14,7 @@ Class Easol_Auth {
         $this->controller = $this->ci->router->fetch_class();
         $this->method = $this->ci->router->fetch_method();
 
-        $this->auth = $this->ci->config->item($this->controller, 'auth');
-
-
+        
         self::$userInfo=$this->ci->session->userdata();
         if($this->ci->session->userdata('logged_in') == true)
         {
@@ -29,6 +27,17 @@ Class Easol_Auth {
         }
 
         if ($this->controller == 'home') return true;
+
+        $this->args = [];
+        $index = strpos($this->ci->uri->uri_string(), $this->controller);
+        $args = explode("/", $this->ci->uri->uri_string());
+        foreach ($args as $k=>$v) {
+            if ($k <= $index + 1) continue;
+            $this->args[] = $v;
+        }
+
+        $this->role = $this->user_role();
+
 
         if(!($this->controller == 'schools' && $this->method == 'choose') && self::userdata('SchoolId') == false  ) {
             return redirect('schools/choose');
@@ -45,14 +54,83 @@ Class Easol_Auth {
 
     }
 
-	public function has_permission() {
+	public function has_permission($controller=null, $method=null) {
 
-        print_r($this->ci->uri->ruri_to_assoc());
+        if (!$controller) $controller = $this->controller;
+        if (!$method) $method = $this->method;
 
-		if (empty($this->auth)) return false;
+        $this->auth_config = $this->ci->config->item($controller, 'auth');
 
-        return true;
+        // auth_config is the array for the current controller in config/auth.php
+        if (empty($this->auth_config)) return false;
+
+        $auth_config = $this->auth_config[$method];
+        if (empty($auth_config)) {
+            foreach ($this->auth_config as $k=>$v) {
+                if (strpos($k, "*") !== false) {
+                    $auth_config = $v;
+                    break;
+                }
+            }
+        }
+
+        // at that point, auth_config is the array for current controller and method. If not found, it is the array for "*"
+        if (is_array($auth_config) && $auth_config[$this->role]) {
+            if (is_array($auth_config[$this->role]['condition'])) {
+               
+                foreach ($auth_config[$this->role]['condition'] as $function) {
+                    if (!method_exists($this, $function)) {
+                        return false;
+                    }
+                    elseif (!$this->$function()) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            else return (bool)$auth_config[$this->role];
+        }
+        elseif ($auth_config == "*") return true;
+
+        return false;
 	}
+
+    public function user_has_school() {
+
+        $StudentUSI = $this->args[0];
+        if (!$StudentUSI) return true;
+
+        $student = Model\Edfi\Student::find($StudentUSI);
+
+        foreach ($student->School() as $school) {
+            if ($school->SchoolId == self::userdata('SchoolId')) return true;
+        }
+
+        return false;
+    }
+
+    public function report_has_access($ReportId = null) {
+
+        if (!$ReportId) $ReportId = $this->args[0];
+        if (!$ReportId) return true;
+
+        if (in_array($this->role, ['System Administrator', 'Data Administrator'])) return true;
+
+        $report = Model\Easol\Report::find($ReportId);
+
+        foreach ($report->ReportAccess() as $access) {
+            if ($access->RoleTypeId == self::userdata('RoleId')) return true;
+        }
+
+        return false;
+    }
+
+
+    public function user_role($StaffUSI=null) {
+        if (!$StaffUSI) $StaffUSI = $this->ci->session->userdata('StaffUSI');
+        $user = Model\Easol\StaffAuthentication::find($StaffUSI);
+        return $user->Role()->RoleTypeName;
+    }
 
  
     /**
