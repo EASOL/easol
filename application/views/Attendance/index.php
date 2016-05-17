a553a9717f8ba4b6ea87baa3a3676aa56053e443
Regis Camimura <regiscamimura@gmail.com>
2016-05-17 11:34:20 -0300

    #350 - Fixed links for attendance and assessment tabs

--- application/views/Attendance/index.php
+++ application/views/Attendance/index.php
@@ -53,7 +53,7 @@
                       <?php if (isset($results) and !empty($results)): ?>
                         <?php foreach ($results as $student => $data) : foreach ($data as $year => $v) : ?>
                           <tr>
-                            <td><?php echo anchor('student/profile/'.$v['StudentUSI'], $v['Name']); ?></td>
+                            <td><?php echo anchor('student/attendance/'.$v['StudentUSI'], $v['Name']); ?></td>
                             <td><?php echo $v['GradeLevel'] ?></td>
                             <td><?php echo (isset($v['In Attendance'])) ? $v['In Attendance'] : '-';  ?></td>
                             <td><?php echo (isset($v['Excused Absence'])) ? $v['Excused Absence'] : '-';  ?></td>
