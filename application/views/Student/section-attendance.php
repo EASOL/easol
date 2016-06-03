d104fa21a04113253b99fae65dd97f609a00acee
Regis Camimura <regiscamimura@gmail.com>
2016-06-03 15:12:44 -0300

    #350 - Modified the attendance student tab to match records from attendance view

---
+++ application/views/Student/section-attendance.php
@@ -0,0 +1,39 @@
+<?php
+$attendances = $student->getAttendance();
+if (empty($attendances)) {
+    $this->load->view('no_results_found');
+} else { ?>
+    <div class="col-md-12">
+        <table id="student-attendance-table" class="table table-striped table-bordered table-widget">
+            <thead>
+                <tr>
+                    <th>Period</th>
+                    <th>Course Code</th>
+                    <th>Section Code</th>
+                    <th>Present</th>
+                    <th>Tardy</th>
+                    <th>Absent</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php  foreach($attendances as $attendance){ ?>
+                <tr>
+                    <td><?php
+
+                    if(strpos($attendance->ClassPeriodName, " - ")!== false)
+                     list($pCode,$pName) = explode(' - ', $attendance->ClassPeriodName);
+                    else
+                     $pCode = $attendance->ClassPeriodName;
+                    echo $pCode;
+                     ?></td>
+                    <td><?= $attendance->LocalCourseCode ?></td>
+                    <td><?= anchor('sections/details/'.$attendance->id, $attendance->UniqueSectionCode, 'target="_blank"'); ?></td>
+                    <td><?= $attendance->Present ?></td>
+                    <td><?= $attendance->Tardy ?></td>
+                    <td><?= $attendance->Absence ?></td>
+                </tr>
+            <?php }  ?>
+            </tbody>
+        </table>
+    </div>
+<?php } ?>
