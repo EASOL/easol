2b2ee48eaa6582dd4c03df85ed6d099322dc03e2
Regis Camimura <regiscamimura@gmail.com>
2016-06-09 11:44:00 -0300

    #351 - Analytics server-side filters on school year to preserve the key value

--- application/modules/analytics/views/index.php
+++ application/modules/analytics/views/index.php
@@ -24,7 +24,7 @@
                               <select name="year" class="form-control">
                                    <option value="">All Years</option>
                                    <?php foreach($years as $k => $v): ?>
-                                        <option value="<?php echo $v; ?>" <?php if($filters['year'] == $v) {echo "selected";} ?> ><?php echo $v; ?></option>
+                                        <option value="<?php echo $k; ?>" <?php if($filters['year'] == $k) {echo "selected";} ?> ><?php echo $v; ?></option>
                                    <?php endforeach; ?>
                               </select>
                          </div>
