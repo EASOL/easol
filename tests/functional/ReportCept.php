69516700737ec3c4c313f39ad076afeffc06104f
Regis Camimura <regiscamimura@gmail.com>
2016-10-14 14:40:39 -0300

    failed tests for debugging

--- tests/functional/ReportCept.php
+++ tests/functional/ReportCept.php
@@ -2,8 +2,8 @@
 
 $I = new FunctionalTester($scenario);
 $I->wantTo('See the reports index page');
-$I->setHeader('X-Test-User', 'edgar.f91@gmail.com');
-$I->setHeader('X-School', '255901044');
+//$I->setHeader('X-Test-User', 'edgar.f91@gmail.com');
+//$I->setHeader('X-School', '255901044');
 
 $I->amOnPage('/reports/index');
 $I->see('Flex Reports');
