9fe4c76e17dc2eb7807413074208ae9e4289e71e
Regis Camimura <regiscamimura@gmail.com>
2016-10-25 21:58:50 -0200

    basic routes tests

---
+++ tests/functional/HomeCept.php
@@ -0,0 +1,9 @@
+<?php
+
+$I = new FunctionalTester($scenario);
+$I->wantTo('See the reports index page');
+//$I->setHeader('X-Test-User', 'edgar.f91@gmail.com');
+//$I->setHeader('X-School', '255901044');
+
+$I->amOnPage('/');
+$I->see('EASOL Login');
